c0f16790aff05fbf16b0384cf5c6bf540f5f220b
hassanahmadp <hassanahmadp@gmail.com>
2021-08-04 03:34:57 +0500

    header scroll added

--- js/app.js
+++ js/app.js
@@ -1,9 +1,20 @@
 const stairs = document.querySelector(".stairs");
 const nav = document.querySelector(".nav");
 const logo = document.querySelector(".logo");
+const header = document.querySelector("header");
 
+// Menu toggle actions
 stairs.addEventListener("click", () => {
     stairs.classList.toggle("active");
     nav.classList.toggle("active");
     logo.classList.toggle("activeNav");
+});
+
+// onscroll header properties change
+document.addEventListener("scroll", () => {
+    if (pageYOffset > 0) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
 });
