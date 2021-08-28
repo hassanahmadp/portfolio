38a44d65bc4789ce5f545026dbd8f61760e23801
hassanahmadp <hassanahmadp@gmail.com>
2021-08-29 03:36:24 +0500

    contact box made

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 const stairs = document.querySelector(".stairs");
 const nav = document.querySelector(".nav");
+const navBTN = document.querySelector(".nav .nav-button");
 const logo = document.querySelector(".logo");
 const header = document.querySelector("header");
 const preloader = document.querySelector(".loadDelayer");
+const contactOverlay = document.querySelector(".contactOverlay");
 
 // Menu toggle actions
 stairs.addEventListener("click", () => {
@@ -23,4 +25,25 @@ document.addEventListener("scroll", () => {
 // preloader
 window.addEventListener('load', () => {
     preloader.classList.add('get-lost');
+});
+
+// show contact overlay
+navBTN.addEventListener('click', () => {
+    contactOverlay.classList.add('active');
+});
+
+// hide contact overlay
+document.querySelector('.overContact').addEventListener("click", () => {
+    contactOverlay.classList.remove('active');
+});
+
+// coply email to clipboard
+document.querySelector("#gmail").addEventListener('click', () => {
+    const socialAlert = document.querySelector('.socialAlert');
+    navigator.clipboard.writeText('hassanahamadp@gmail.com');
+
+    socialAlert.classList.add('active');
+    setTimeout(() => {
+        socialAlert.classList.remove('active');
+    }, 3000);
 });
